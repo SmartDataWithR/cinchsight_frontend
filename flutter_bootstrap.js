d220add418870b296bdbe6de4b46396c1a5d4468
Bert Gollnick <info@gollnickdata.de>
2025-05-08 08:42:02 +0200

    bugfix: DeepResearch

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2729047473"
+    serviceWorkerVersion: "4051193346"
   }
 });
