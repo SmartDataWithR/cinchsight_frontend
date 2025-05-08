e0bd3ed77af6188d27b160d45b1afe5919b84fe8
Bert Gollnick <info@gollnickdata.de>
2025-05-08 08:19:59 +0200

    bugfix: patent canva

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2688,14 +2688,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "5618ded7beab39dd8ec03679355bc6dd
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "98ea1de089d557218181f3bd06f3169f",
+"flutter_bootstrap.js": "9c150dba058e281bb9cc99d37272635d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "5e6d76e7eb39ce9806c2d3fb5a048e56",
-"/": "5e6d76e7eb39ce9806c2d3fb5a048e56",
-"main.dart.js": "f657c38827371b84cbf46d0e9e77d3fc",
+"index.html": "5274cb11543a5f6cf2f4f21814ff0716",
+"/": "5274cb11543a5f6cf2f4f21814ff0716",
+"main.dart.js": "f3d7220ccc6e66c2822fb2698fe03684",
 "manifest.json": "760e4efbc08f731b0cca0dde02c920b0",
 "version.json": "0b363f3cfbec5dd44423f8e069df45be"};
 // The application shell files that are downloaded before a service worker can
