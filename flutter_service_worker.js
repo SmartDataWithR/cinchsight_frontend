8886677f9774f56902b172fb6a71d4c256452ca5
Bert Gollnick <info@gollnickdata.de>
2025-11-26 08:41:32 +0100

    dashboard bugs solved

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2689,14 +2689,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "4b8eb74dd91268f25794df77ccfd5baf
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "fc6fe4495aa22e30d731d6936913af28",
+"flutter_bootstrap.js": "37c4e440f5f69852d91db94579a35c17",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "d109a283f953f57ff47784be60111bdd",
-"/": "d109a283f953f57ff47784be60111bdd",
-"main.dart.js": "447ae2298c893e5596a5501f5064523d",
+"index.html": "3bd3eb39bec57f06165105abffb5141b",
+"/": "3bd3eb39bec57f06165105abffb5141b",
+"main.dart.js": "ce472f26d191ef14895c0f47d260492d",
 "manifest.json": "760e4efbc08f731b0cca0dde02c920b0",
 "version.json": "0b363f3cfbec5dd44423f8e069df45be"};
 // The application shell files that are downloaded before a service worker can
