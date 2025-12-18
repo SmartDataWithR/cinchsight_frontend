552b466da5bc20e6abd5d4398be60c38a01d1582
Bert Gollnick <info@gollnickdata.de>
2025-12-18 16:57:27 +0100

    several UI changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2689,14 +2689,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "4b8eb74dd91268f25794df77ccfd5baf
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "cfa81d3de3d3e6336721bbbd9a33a6b8",
+"flutter_bootstrap.js": "7d05c1529323721f8ab19a33a5c1f06c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "6195c7d337f09d597738a9ea56f54127",
-"/": "6195c7d337f09d597738a9ea56f54127",
-"main.dart.js": "28e1561481af6840ec89a0d3de94e56b",
+"index.html": "ea29dfdec7fb4657d112858743e41556",
+"/": "ea29dfdec7fb4657d112858743e41556",
+"main.dart.js": "9a244868bc2b0d46627f72130ab7e7c6",
 "manifest.json": "760e4efbc08f731b0cca0dde02c920b0",
 "version.json": "0b363f3cfbec5dd44423f8e069df45be"};
 // The application shell files that are downloaded before a service worker can
